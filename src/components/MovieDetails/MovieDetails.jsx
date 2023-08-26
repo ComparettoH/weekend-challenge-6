6ca637906d3b5e8e94801af7fba6610bdbdc765c
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-26 12:46:25 -0500

    create back home button

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function MovieDetails () {
     const dispatch = useDispatch();
+    const history = useHistory()
+
+    const backHome = () => {
+        history.push('/');
+    }
 
     return (
-        <div>
+        <main>
+            <button onClick={backHome}>Back to List</button>
             <h1>In Movie Details!</h1>
-        </div>
+        </main>
     )
 }
 
